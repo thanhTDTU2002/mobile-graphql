48f74a327eca4ffd731883b17e1bbc484b34a677
thanhTDTU2002 <thanh14147@gmail.com>
2022-12-12 15:19:05 +0700

    get singers

--- schemas/singer/resolvers.js
+++ schemas/singer/resolvers.js
@@ -1,5 +1,27 @@
-const { singers } = require('../../mock-data');
+const { singers, songs } = require('../../mock-data');
+
+const getSongs = (singerId) =>
+	songs.filter((song) => {
+		return song.singers.includes(singerId);
+	});
 
 module.exports = {
-	singers: () => singers,
+	singers: (_, args) => {
+		const { page, limit } = args.options;
+		const startIdx = limit * (page - 1);
+
+		const listSingers = singers.slice(startIdx, startIdx + limit);
+
+		return listSingers.map((singer) => ({
+			...singer,
+			songs: getSongs(singer.id),
+		}));
+	},
+
+	singer: (_, args) => {
+		const id = args.id;
+		const singer = singers.find((singer) => singer.id == id);
+
+		return { ...singer, songs: getSongs(singer.id) };
+	},
 };
